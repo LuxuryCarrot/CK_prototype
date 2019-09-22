ef1827ea021e4a20b1f747908af83995516c5319
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-09-22 14:35:31 +0900

    몬스터 AI

---
+++ 도토리/Prototype/Assets/Scripts/Monster/Monster.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float movePower;
+    Animator animator;
+    Vector3 movement;
+    int movementFlag = 0;
+    bool isChasing;
+    GameObject ChaseTarget;
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        
+        animator = GetComponentInChildren<Animator>();
+
+        StartCoroutine("ChangeMovement");
+
+        
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);
+
+        if (movementFlag == 0)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            animator.SetBool("isMoving", true);
+        }
+        yield return new WaitForSeconds(3.0f);
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ChaseTarget = other.gameObject;
+
+            StopCoroutine("ChangeMovement");
+
+            Debug.Log("isChasing enter");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isChasing = true;
+            animator.SetBool("isMoving", true);
+            Debug.Log("isChasing");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")    
+        {
+            isChasing = false;
+            StartCoroutine("ChangeMovement");
+            Debug.Log("isChasing false");
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        string dist = "";
+
+        if (isChasing)
+        {
+            Vector3 playerPos = ChaseTarget.transform.position;
+
+            if (playerPos.x < transform.position.x)
+            {
+                dist = "Left";
+            }
+            else if (playerPos.x > transform.position.x)
+            {
+                dist = "Right";
+            }
+        }
+        else
+        {
+            if (movementFlag == 1)
+            {
+                dist = "Left";
+            }
+            else if (movementFlag == 2)
+            {
+                dist = "Right";
+            }
+        }
+
+        if (dist == "Left")
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (dist == "Right")
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}
