6af2fe3d12184f71707f5ac2914a32a925ffe86c
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-10-08 05:20:09 +0900

    ThornBush

--- 도토리/Prototype/Assets/Scripts/ThornBush.cs
+++ 도토리/Prototype/Assets/Scripts/ThornBush.cs
@@ -17,7 +17,7 @@ public class ThornBush : MonoBehaviour
     {
         
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
