75ad56ec998106a472aaf8c15c138daa17d18d76
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-10-08 06:23:31 +0900

    UI setting

---
+++ 도토리/Prototype/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    GameObject PausePanel;
+    private void Awake()
+    {
+        PausePanel = GameObject.FindGameObjectWithTag("PausePanel");
+    }
+
+    private void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+    public void OnClickPause()
+    {
+        PausePanel.SetActive(true);
+    }
+    public void OnClickContinue()
+    {
+        PausePanel.SetActive(false);
+    }
+    public void OnClickExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("http://google.com");
+#else
+        Application.Quit();
+#endif
+    }
+}
--- 도토리/Prototype/Assets/Scripts/GameManager.cs
+++ 도토리/Prototype/Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     private static GameManager instance;
     public static GameManager Instance
     {
