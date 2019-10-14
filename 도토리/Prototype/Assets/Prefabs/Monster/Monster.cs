bfdedee133df496fab4fbb819284addcb5d5e396
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-10-15 01:38:00 +0900

    dd

---
+++ 도토리/Prototype/Assets/Prefabs/Monster/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimalName
+{
+    FOX,
+    FROG,
+    RACCOON
+}
+
+public class Monster : MonoBehaviour
+{
+    public AnimalName animalname;
+
+}
