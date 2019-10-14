9bd0f5e67438f2f9837a09fa1c5ff47bbd4d6a62
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2019-10-15 01:38:53 +0900

    adgadg

--- 도토리/Prototype/Assets/Scripts/Player/PlayerATTACK.cs
+++ 도토리/Prototype/Assets/Scripts/Player/PlayerATTACK.cs
@@ -1,19 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Reflection;
 using System;
 
 
 public class PlayerATTACK : PlayerFSMController
 {
-    DamageCalc dele;
-
-    IPassiveSkill firePassive = new FireSkill();
-    IPassiveSkill waterPassive = new WaterSkill();
-    IPassiveSkill grassPassive = new GrassSkill();
-
-
     public override void BeginState()
     {
         base.BeginState();
@@ -22,25 +14,6 @@ public class PlayerATTACK : PlayerFSMController
     // Update is called once per frame
     void Update()
     {
-        switch (controller.stat.curWeaponProperty)
-        {
-            case ElementalProperty.Fire:
-                dele = new DamageCalc(firePassive.DamageMUL);
-                controller.stat.finalDamage = firePassive.EmpowerInWeapon(controller.stat.weaponDamage, firePassive.GetBuff(), dele);
-                break;
-            case ElementalProperty.Water:
-                dele = new DamageCalc(waterPassive.DamageMUL);
-                controller.stat.finalDamage = waterPassive.EmpowerInWeapon(controller.stat.weaponDamage, waterPassive.GetBuff(), dele);
-                break;
-            case ElementalProperty.Grass:
-                dele = new DamageCalc(grassPassive.DamageMUL);
-                controller.stat.finalDamage = grassPassive.EmpowerInWeapon(controller.stat.weaponDamage, grassPassive.GetBuff(), dele);
-                break;
-            default:                                   //None
-                controller.stat.finalDamage = controller.stat.weaponDamage;
-                break;
-        }
-
         if (controller.curAttackAnimSpeed >= PlayerController.MAX_ATTACK_ANIM_TIME)
         {
             if (controller.attackDir == 1)                                                                     //attack up
--- 도토리/Prototype/Assets/Scripts/Player/WeaponSword.cs
+++ 도토리/Prototype/Assets/Scripts/Player/WeaponSword.cs
@@ -16,6 +16,12 @@ public class WeaponSword : MonoBehaviour
 
     bool isMonsterCheck;
 
+    DamageCalc dele;
+
+    IPassiveSkill firePassive = new FireSkill();
+    IPassiveSkill waterPassive = new WaterSkill();
+    IPassiveSkill grassPassive = new GrassSkill();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,13 +38,22 @@ public class WeaponSword : MonoBehaviour
             {
                 if (player.curAttackAnimSpeed >= PlayerController.MAX_ATTACK_ANIM_TIME)
                 {
-                    //대미지 텍스트를 몬스터 위치에 생성
+                    /*
+                    var animal = monster.get; 상태 enum
+                     */
+                    stat.finalDamage = stat.weaponDamage;
 
+
+
+
+                    //대미지 텍스트를 몬스터 위치에 생성
                     var cloneText = Instantiate(GameManager.Instance.ui.prefab_floating_text,
                     Camera.main.WorldToScreenPoint(new Vector3(monster.position.x, monster.position.y + 1f, monster.position.z)), Quaternion.identity);
 
                     cloneText.GetComponent<FloatingDamageText>().text.text = Convert.ToString(stat.finalDamage);
                     cloneText.transform.SetParent(GameManager.Instance.ui.canvas.transform);
+                    ////////////////////////////////////////////////////////////////////////////////////
+
 
                     Debug.Log("Player Damage : " + stat.finalDamage);
                     monster.SendMessage("ApplyDamage", stat.finalDamage);
@@ -46,6 +61,41 @@ public class WeaponSword : MonoBehaviour
                 }
             }
         }
+    }
+
+
+    public float DamageCalc(ref AnimalName animal)
+    {
+        float damage = 0;
+
+        switch(animal)
+        {
+            case AnimalName.FOX:
+                if(player.stat.curWeaponProperty == ElementalProperty.Water)
+                {
+                    dele = new DamageCalc(waterPassive.DamagePercent);
+                    damage = waterPassive.EmpowerInWeapon(stat.weaponDamage, 50f, dele);
+
+                }
+                break;
+            case AnimalName.FROG:
+                if(player.stat.curWeaponProperty==ElementalProperty.Grass)
+                {
+                    dele = new DamageCalc(grassPassive.DamagePercent);
+                    damage = grassPassive.EmpowerInWeapon(stat.weaponDamage, 50f, dele);
+
+                }
+                break;
+            case AnimalName.RACCOON:
+                if(player.stat.curWeaponProperty==ElementalProperty.Fire)
+                {
+                    dele = new DamageCalc(firePassive.DamagePercent);
+                    damage = firePassive.EmpowerInWeapon(firePassive.GetDamage(stat.weaponDamage), 50f, dele);
+                }
+                break;
+        }
+
+        return damage;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
